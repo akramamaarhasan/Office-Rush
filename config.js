c4344010688ab5b7a4005b2f47618e0a3d661b74
Akram Hasan <akram01323@gmail.com>
2026-09-25 13:33:12 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 window.__ENV__ = window.__ENV__ || {};
-window.__ENV__.FIREBASE_API_KEY = window.__ENV__.FIREBASE_API_KEY || "AIzaSyD4ys5vgvzAkxi6el9SW2v8ljdL2ECxJ5g";
+window.__ENV__.FIREBASE_API_KEY = window.__ENV__.FIREBASE_API_KEY || "AIzaSyBY0EYts6ggJ2sVR78vNxy2EdnGjjmgeWs";
